5e3fef286f9e88ef7cccb421ce6005fa5f887e29
mamurjor sujon <mamurjor.sujon@gmail.com>
2021-06-28 16:11:57 +0600

    dashboard design

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@
 $(document).ready(function(){
 
     // left side
-    const leftScrollBar = document.querySelector('.left-panel');
+    const leftScrollBar = document.querySelector('.right-panel');
     new PerfectScrollbar(leftScrollBar);
+
+    $('#user-info-btn').on('click', function(){
+        alert();
+        // $('#user-info-show').show();
+    })
+
 
 });
