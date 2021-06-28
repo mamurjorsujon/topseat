dc67577691db2150024befda113a485b9a3abc08
mamurjor sujon <mamurjor.sujon@gmail.com>
2021-06-28 20:05:13 +0600

    dashboard index page fixed

--- js/modal-slides.js
+++ js/modal-slides.js
@@ -85,8 +85,42 @@ $(document).ready(function(){
         image: {
             titleSrc: 'title'
           }
-      });
+    });
 
+
+    // single page related topseat
+    $(".topseat-single-related-slider").slick({
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        prevArrow: $(".prev-related"),
+        nextArrow: $(".next-related"),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                    infinite: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
 
   });
 
