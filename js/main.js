1efb5b8d55830a464f0e644cc680e0d625952652
mamurjor sujon <mamurjor.sujon@gmail.com>
2021-06-29 12:48:33 +0600

    user dashboard show

--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@
 const leftScrollBar = document.querySelector('.right-panel');
 new PerfectScrollbar(leftScrollBar);
 
-
-function openDashboard(){
+// open dashboard items
+function openLeftDashboard(){
     document.getElementById("user-left-dashboard").style.display = "block";
 }
 
+// close dashboard items
+function leftPanelClose(){
+    document.getElementById("user-left-dashboard").style.display = "none";
+}
 
 function dashboardCloseBtn(){
-    document.getElementById("user-dashboard-show").style.display = "none";
+    document.getElementById('user-dashboard-show').style.display = 'none';
 }
+
